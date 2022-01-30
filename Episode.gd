aa748b7095b3dda546cc04d0e6552a0ce94404b0
Haley Wakamatsu <japanyoshi777@gmail.com>
2022-01-30 14:42:32 +0900

    Disable debug features

--- Episode.gd
+++ Episode.gd
@@ -28,19 +28,19 @@ func _ready():
 	q_box.connect("question_done", self, "load_next_question")
 	q_box.hud = hud
 	question_number = 0
-	if not R.pass_between.has("episode_name"):
-		# we're debugging
-		R.pass_between.episode_name = "demo"
-		question_number = 0
-		load_next_question()
-		return
-	elif R.pass_between.episode_name == "demo":
-		# still debug for now
-		DEBUG = true
-		question_number = 6
-		episode_data = Loader.load_episode(R.pass_between.episode_name)
-		load_next_question()
-		return
+#	if not R.pass_between.has("episode_name"):
+#		# we're debugging
+#		R.pass_between.episode_name = "demo"
+#		question_number = 0
+#		load_next_question()
+#		return
+#	elif R.pass_between.episode_name == "demo":
+#		# still debug for now
+#		DEBUG = true
+#		question_number = 6
+#		episode_data = Loader.load_episode(R.pass_between.episode_name)
+#		load_next_question()
+#		return
 	episode_data = R.pass_between.episode_data
 	$Cutscenes/Round2.scale = Vector2(0, 1)
 	call_deferred("play_intro")
