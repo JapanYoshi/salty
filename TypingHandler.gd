952886db5971adc46876c7fa2aa0ee13f97fb6e6
Haley Wakamatsu <japanyoshi777@gmail.com>
2022-09-06 17:26:23 +0900

    Check if it's connected before disconnecting

--- TypingHandler.gd
+++ TypingHandler.gd
@@ -119,7 +119,9 @@ func submit():
 	can_type = false
 	emit_signal("text_confirmed", tbox.get_text().strip_edges())
 	$Anim.play("Hide")
-	Ws.disconnect('remote_typing', self, 'remote_typing')
+	print(Ws.get_signal_connection_list('remote_typing'))
+	if self in Ws.get_signal_connection_list('remote_typing'):
+		Ws.disconnect('remote_typing', self, 'remote_typing')
 
 func reset_state():
 	which_player = -1
