1b228278c78139684dee999cfc92619f195c1e8b
Haley Wakamatsu <japanyoshi777@gmail.com>
2022-11-17 20:46:00 +0900

    Update animation

--- logo/LogoContainer.gd
+++ logo/LogoContainer.gd
@@ -1,5 +1,6 @@
 extends Control
 var title: PackedScene = preload("res://Title.tscn")
+onready var anim: AnimationPlayer = $AnimationPlayer
 # Animated logo for hai!touch Studios.
 
 # Called when the node enters the scene tree for the first time.
@@ -9,20 +10,25 @@ func _ready():
 # Called when the logo is finished animating.
 # Also called when the user skips the logo.
 func _on_AnimationPlayer_animation_finished(anim_name):
-	if anim_name == "haitouch":
-		$AnimationPlayer.play("jackbox")
-	else:
+	if anim_name == "fadeout":
+		anim.play("haitouch")
+	elif anim_name == "haitouch":
+		anim.play("ydkj")
+	elif anim_name == "ydkj":
 		get_tree().change_scene_to(title)
 
 # Called when the user skips the logo.
 func skip():
-	var anim_name = $AnimationPlayer.current_animation
+	var anim_name = anim.current_animation
 	if anim_name == "":
 		S.play_sfx("key_press")
-		$AnimationPlayer.play("haitouch")
+		anim.play("fadeout")
+	elif anim_name == "fadeout":
+		pass
 	else:
-		$AnimationPlayer.seek(100,true)
-		_on_AnimationPlayer_animation_finished(anim_name)
+		if anim.get_current_animation_position() > 0.25:
+			anim.seek(100,true)
+			_on_AnimationPlayer_animation_finished(anim_name)
 
 func _input(event):
 	if "pressed" in event and event.pressed:
