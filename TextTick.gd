38f364df83c12b4b25ddb52ea1320529620968f7
Haley Wakamatsu <japanyoshi777@gmail.com>
2022-11-27 20:46:57 +0900

    Incorporate countdown meter to AOS

--- TextTick.gd
+++ TextTick.gd
@@ -23,9 +23,23 @@ onready var tween = $Tween
 onready var gib_q_box = $GibQBox
 onready var gib_q = $GibQBox/GibQ
 onready var gib_category = $GibCategory
-onready var gib_clue1 = $GibClue1
-onready var gib_clue2 = $GibClue2
-onready var gib_clue3 = $GibClue3
+onready var gib_clues = [
+	$GibClue1,
+	$GibClue2,
+	$GibClue3,
+]
+onready var gib_bars = [
+	$GibBar1,
+	$GibBar2,
+	$GibBar3,
+	$GibBar4,
+]
+const gib_bar_text = [
+	" 1st clue in %s″...",
+	" 2nd clue in %s″...",
+	" 3rd clue in %s″...",
+	" Time’s up in %s″...",
+]
 onready var gib_a_box = $GibABox
 onready var gib_a = $GibABox/GibA
 
@@ -37,10 +51,13 @@ func _ready():
 	gib_anim.play("reset")
 	pass
 
-func countdown():
+func countdown(first_time: bool = false):
 	if mode == "T":
 		anim.play("countdown")
 	else:
+		if first_time:
+			gib_bars[0].flip_in(tween, 0.0, 10.0)
+			tween.start()
 		anim.play("countdown_gib")
 
 func countdown_pause(paused: bool = true):
@@ -61,9 +78,8 @@ func init_thousand():
 	dollars.add_color_override("font_color", Color(64/255.0, 36/255.0, 4/255.0, 126/255.0))
 	thou_anim.play("reset")
 	gib_q.set_text("")
-	gib_clue1.bbcode_text = ""
-	gib_clue2.bbcode_text = ""
-	gib_clue3.bbcode_text = ""
+	for el in gib_clues:
+		el.bbcode_text = ""
 	$GibACont/GibA.bbcode_text = ""
 	$ColorRect.modulate = Color.white
 	if !R.cfg.cutscenes:
@@ -83,12 +99,20 @@ func init_gibberish(category, question, clue1, clue2, clue3, answer, is_round2):
 	gib_q.bbcode_text = "[center]" + question + "[/center]"
 	gib_q_box.rect_scale.y = 0
 	gib_q_box.rect_position.y = question_y
-	gib_clue1.bbcode_text = clue1
-	gib_clue1.rect_scale.y = 0
-	gib_clue2.bbcode_text = clue2
-	gib_clue2.rect_scale.y = 0
-	gib_clue3.bbcode_text = clue3
-	gib_clue3.rect_scale.y = 0
+	
+	gib_clues[0].bbcode_text = clue1
+	gib_clues[0].rect_scale.y = 0
+	gib_clues[1].bbcode_text = clue2
+	gib_clues[1].rect_scale.y = 0
+	gib_clues[2].bbcode_text = clue3
+	gib_clues[2].rect_scale.y = 0
+	
+	for i in range(4):
+		gib_bars[i].rect_scale.y = 0
+		gib_bars[i].setup(
+			anim, gib_bar_text[i]
+		)
+	
 	gib_a.bbcode_text = "[center]" + answer + "[/center]"
 	gib_a_box.rect_scale.y = 0
 	gib_a_box.rect_position.y = question_y
@@ -119,7 +143,8 @@ func show_price():
 	anim.play("entry")
 
 func update_price():
-	count = round(anim.current_animation_position * 2) # tick twice every second
+	count = round(anim.current_animation_position * 2)
+	# TQQs tick twice every second. AOSs tick once every second, but goes in 2s.
 	var dollar_tween = $PriceBox/Tween
 	dollar_tween.stop_all()
 	dollar_tween.interpolate_property(
@@ -133,27 +158,36 @@ func update_price():
 	dollar_tween.start()
 	value = 0
 	if mode == "T":
-		if count <= 100:
+		if count <= 100.0: # For the first 50 seconds, exponential
 			value = int(floor(max_value * pow(10, -count / 20.0)))
-		elif count < 120:
-			value = (120 - count) / 2
-		elif count == 120:
+		elif count < 120.0: # For the last 10 seconds, linear
+			value = int((120.0 - count) * 0.5)
+			# Play countdown sfx for last 5 seconds
+			if count >= 110.0 and int(count) & 1 == 0:
+				S.play_sfx("time_close")
+		elif count == 120.0: # Time's up
 			value = 0
 			anim.stop(false)
 			emit_signal("checkpoint", 0)
 		dollars.set_text(R.format_currency(value, true, 3))
 		print(str(count) + ": The question is worth " + str(value) + " points.")
 	elif mode == "G":
-		value = int(max_value * (80 - count) / 80.0)
-		if count == 20:
-			emit_signal("checkpoint", 0)
-		elif count == 40:
-			emit_signal("checkpoint", 1)
-		elif count == 60:
-			emit_signal("checkpoint", 2)
-		elif count == 80:
-			emit_signal("checkpoint", 3)
-			anim.stop(false)
+		value = int(max_value * (80.0 - count) / 80.0)
+		match count:
+			20.0:
+				emit_signal("checkpoint", 0)
+			40.0:
+				emit_signal("checkpoint", 1)
+			60.0:
+				emit_signal("checkpoint", 2)
+			70.0, 72.0, 74.0, 76.0, 78.0:
+				S.play_sfx("time_close")
+			80.0:
+				emit_signal("checkpoint", 3) # signals to Standard.gd
+				S.play_sfx("time_up")
+				anim.stop(false)
+#			_:
+#				pass
 		dollars.set_text(R.format_currency(value, true))
 		print(str(count) + ": The question is worth " + str(value) + " points.")
 
@@ -211,23 +245,32 @@ func skip_thou_intro():
 	yield(thou_anim, "animation_finished");
 	$ThouTute.hide()
 
-func gib_clue(index: int, start: bool = true):
-	var el = [gib_clue1, gib_clue2, gib_clue3][index]
+# Show the index-th clue and flip in the next countdown bar.
+# DOES NOT start the tween.
+func gib_clue(index: int):
+	var el = gib_clues[index]
 	tween.interpolate_property(
 		el, "rect_scale", el.rect_scale, Vector2.ONE, 0.2, Tween.TRANS_CUBIC, Tween.EASE_OUT
 	)
 	tween.interpolate_property(
 		el, "modulate", el.modulate, Color.white, 0.01, Tween.TRANS_CUBIC, Tween.EASE_OUT
 	)
-	if !start: return
-	tween.start()
 
 func gib_reveal():
-	print("gib_clue1.rect_scale has type %f and Vector2.ONE has type %f" % [typeof(gib_clue1.rect_scale), typeof(Vector2.ONE)])
-	print("gib_clue1.modulate has type %f and Color.white has type %f" % [typeof(gib_clue1.modulate), typeof(Color.white)])
-	gib_clue(0, false)
-	gib_clue(1, false)
-	gib_clue(2, false)
+#	print("gib_clue1.rect_scale has type %f and Vector2.ONE has type %f" % [typeof(gib_clue1.rect_scale), typeof(Vector2.ONE)])
+#	print("gib_clue1.modulate has type %f and Color.white has type %f" % [typeof(gib_clue1.modulate), typeof(Color.white)])
+	gib_clue(0)
+	gib_clue(1)
+	gib_clue(2)
+	for el in gib_bars:
+		if el.time_limit != 0.0:
+			tween.interpolate_property(
+				el, "rect_scale", el.rect_scale, Vector2.RIGHT, 0.2, Tween.TRANS_CUBIC, Tween.EASE_OUT
+			)
+			tween.interpolate_property(
+				el, "modulate", el.modulate, Color.transparent, 0.01, Tween.TRANS_CUBIC, Tween.EASE_OUT, 0.19
+			)
+			break
 	tween.interpolate_property(
 		gib_q_box, "rect_scale", gib_q_box.rect_scale, Vector2.RIGHT, 0.2, Tween.TRANS_CUBIC, Tween.EASE_OUT
 	)
@@ -246,7 +289,7 @@ func gib_reveal():
 	tween.interpolate_property(
 		gib_a_box, "modulate", gib_a_box.modulate, Color.white, 0.01, Tween.TRANS_CUBIC, Tween.EASE_OUT
 	)
-	print("Alright, should be fine to tween these. Play it")
+#	print("Alright, should be fine to tween these. Play it")
 	tween.start()
 
 func thou_tute_set_value():
@@ -258,7 +301,12 @@ func thou_tute_set_value():
 func _on_TextTick_checkpoint(checkpoint):
 	print("DEBUG: Checkpoint %d reached." % checkpoint)
 	if mode == "G":
+		if checkpoint >= 3: return
 		gib_clue(checkpoint)
+		gib_bars[checkpoint + 1].flip_in(
+			tween, float(checkpoint + 1) * 10.0, float(checkpoint + 2) * 10.0
+		)
+		tween.start()
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name in ["gib_logo", "thou_logo"]:
