96f9c2fa4c185b34dd046fe74485d478d88383ee
Haley Wakamatsu <japanyoshi777@gmail.com>
2022-12-07 16:55:10 +0900

    Error screen impvorements
    Clickable links and resizing error window

--- Error.gd
+++ Error.gd
@@ -1,7 +1,12 @@
 extends ColorRect
 
 func set_reason(text):
-	$ColorRect/Reason.set_text(text)
+	$ScreenStretch/ColorRect/VBoxContainer/Reason.set_text(text)
+
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Title.tscn")
+
+
+func _on_Footer_meta_clicked(meta):
+	OS.shell_open(meta)
