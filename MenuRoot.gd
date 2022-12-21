dd5ae83cb4e7b4d63d196fbec4bd17e9e354f476
Haley Wakamatsu <japanyoshi777@gmail.com>
2022-12-21 12:02:27 +0900

    menuroot improvements

--- MenuRoot.gd
+++ MenuRoot.gd
@@ -1,6 +1,6 @@
 extends Control
 
-onready var current_page = $ScreenStretch/Episodes
+onready var current_page
 var first_page = preload("res://Episodes.tscn")
 var signup = preload("res://signup.tscn")
 onready var tween = $Tween
@@ -13,6 +13,9 @@ signal next_question_please
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	current_page = first_page.instance()
+	$ScreenStretch.add_child(current_page, true)
+	$ScreenStretch.move_child(current_page, 1)
 	current_page.menu_root = self
 	print("MenuRoot readying...")
 	S.play_multitrack("signup_base", 1, "signup_extra", 0, "signup_extra2", 0)
@@ -76,6 +79,7 @@ const RAND_PREFIX = "RNG_"
 var load_order = []
 
 func load_episode(ep):
+	Loader.reset_question_text()
 #	var LOG_FILE = File.new()
 #	LOG_FILE.open("user://LOG.txt", File.WRITE)
 #	LOG_FILE.close()
@@ -169,23 +173,36 @@ func load_episode(ep):
 			var q = search_order[i]
 			q_id[q] = shorties[i]
 		print("Fill rest with shorties:\n", q_id)
-	R.pass_between.episode_data.question_id = q_id
+	for i in range(len(q_id)):
+		R.pass_between.episode_data.question_id[i] = q_id[i]
 	print("MenuRoot: Question IDs for episode ", ep, " are ", R.pass_between.episode_data.question_id, ".")
-	# Load the questions. If local copies exist, load them first.
+	# Load the questions.
+	# If local copies exist, load them immediately.
 	load_order = []
 	for q in range(QUESTION_COUNT):
 		if Loader.is_question_cached(q_id[q]):
-			call_deferred("_load_question", q_id[q])
+			print("MenuRoot: Question already cached: ", q_id[q])
+			_load_question(q_id[q])
 		else:
+			print("MenuRoot: Question must be downloaded: ", q_id[q])
 			load_order.push_back(q_id[q])
-	# Now start downloading and importing the question files.
+
+	assert(len(Loader.question_texts.keys()) == 13 - len(load_order), "Only %d question texts are preemptively loaded." % len(Loader.question_texts.keys()))
+	print("MenuRoot: List of questions that must be downloaded: ", load_order)
+	# These questions do not exist locally.
+	# Start downloading and importing the question files.
 	time_start = OS.get_ticks_msec()
 	load_start = 0
 	question_pack_is_downloaded = false
 	_update_loading_progress(0, len(load_order))
 	for q in range(len(load_order)):
-		async_load_question(q_id[q])
+		yield(get_tree(), "idle_frame")
+		print("MenuRoot: DOWNLOAD_QUESTION")
+		download_question(load_order[q]) # this was the issue
+		print("MenuRoot: YIELD START")
 		yield(self, "next_question_please")
+		assert(len(Loader.question_texts.keys()) == 14 - len(load_order) + q, "Only %d question texts are preemptively loaded." % len(Loader.question_texts.keys()))
+		print("MenuRoot: YIELD END")
 		if cancel_loading:
 			return
 		_update_loading_progress(q + 1, len(load_order))
@@ -194,71 +211,34 @@ func load_episode(ep):
 	if waiting_for_game_start:
 		start_game()
 
-# Request the game to start.
-func start_game():
-	if !question_pack_is_downloaded:
-		# Wait until the question files are loaded.
-		# This function will be called again once it's done.
-		waiting_for_game_start = true
-		current_page.get_node("MouseMask").color = Color(0, 0, 0, 0.5)
-		current_page.get_node("LoadingPanel").show()
-		return
-	# Fade the music. (Takes 0.5 seconds.)
-	S.play_track(0, 0.1)
-	S.play_track(1, 0.1)
-	S.play_track(2, 0.1)
-	# Transition effect.
-	#self.rect_pivot_offset = Vector2(640,360)
-	tween.interpolate_property(self, "modulate", Color.white, Color.black, 0.5, Tween.TRANS_CUBIC, Tween.EASE_OUT)
-	tween.interpolate_property(self, "rect_scale", Vector2.ONE, Vector2.ONE * 1.1, 0.5, Tween.TRANS_CUBIC, Tween.EASE_OUT)
-	tween.interpolate_property(current_page.get_node("LoadingPanel"), "rect_scale", Vector2.ONE, Vector2.ONE * 1.25, 0.5, Tween.TRANS_CUBIC, Tween.EASE_OUT)
-	tween.start()
-	yield(tween, "tween_all_completed")
-	get_tree().call_deferred("change_scene", "res://Episode.tscn")
 
 onready var http_request = $HTTPRequest
 
-func async_load_question(q):
-	print("async_load_question(%s)" % q)
-	if Loader.is_question_cached(q):
-		print("Question cache exists locally. Loading...")
-		call_deferred("_load_question", q)
-	else:
-		print("Downloading question data...")
-		var url = "https://haitouch.ga/me/salty/%s.pck" % q
-		# Create an HTTP request node and connect its completion signal.
-		var download_file = Loader.q_cache_path + "%s.pck" % q
-		http_request.set_download_file(download_file)
-		http_request.download_chunk_size = 262144
-		http_request.connect("request_completed", self, "_http_request_completed", [q])
-		# Perform the HTTP request. The URL below returns a PNG image as of writing.
-		var error = http_request.request(url)
-		if error != OK:
-			push_error("An error occurred while making the HTTP request.")
-			return
+func download_question(q):
+	print("download_question(%s)" % q)
+	var url = Loader.question_url + Loader.q_cache_filename % q
+	# Create an HTTP request node and connect its completion signal.
+	var download_file = Loader.q_cache_path + Loader.q_cache_filename % q
+	while http_request.get_http_client_status() != HTTPClient.STATUS_DISCONNECTED:
+		# wait while the HTTP client is still downloading
+		yield(get_tree().create_timer(1.0), "timeout")
+		print("download_question(%s): " % q, "Waiting for previous request to finish")
+	print("download_question(%s): " % q, "Previous request has finished")
+	http_request.set_download_file(download_file)
+	http_request.download_chunk_size = 262144
+	http_request.disconnect("request_completed", self, "_http_request_completed")
+	http_request.connect("request_completed", self, "_http_request_completed", [q])
 
-# show player how much data is loaded
-func _update_loading_progress(partial, load_count):
-	var time = OS.get_ticks_msec()
-	# total time / total size == partial time / partial size.
-	# move total size to right hand side and get
-	# total time == partial time * total size / partial size..
-	var eta = -1
-	if partial != 0:
-		eta = (
-			1.0 * (time - time_start) * # partial time
-			(load_count - load_start) / # total size
-			(partial - load_start) # partial size
-		) - (time - time_start)  # subtract elapsed time from total
-	current_page.update_loading_progress(
-		partial + QUESTION_COUNT - load_count,
-		QUESTION_COUNT,
-		int(eta)
-	)
+	var error = http_request.request(url)
+	if error != OK:
+		push_error("An error occurred while making the HTTP request.")
+	print("Request sent to download (%s)" % q)
+	return
+
 
 # Called when the HTTP request is completed.
 func _http_request_completed(result, response_code, headers, body, q):
-	prints("MenuRoot._http_request_completed(", result, response_code, ")")
+	prints("MenuRoot._http_request_completed(", response_code, ", ", q, ")")
 	http_request.disconnect("request_completed", self, "_http_request_completed")
 	if result != HTTPRequest.RESULT_SUCCESS:
 		var error_message = "The HTTP request for question ID %s did not succeed. Error code: %d — " % [q, result]
@@ -295,23 +275,29 @@ func _http_request_completed(result, response_code, headers, body, q):
 	Loader.append_question_cache(q)
 	_load_question(q)
 
+
+## Called after HTTP request is completed or local file is confirmed to exist.
 func _load_question(q):
-	# not sure what to do when loaded...
+	print("_load_question(%s)" % q)
 	# check file existence
 	if Loader.is_question_cached(q):
-		var file = File.new()
-		var success: bool = Loader.load_cached_question(q)
+		var success: bool = Loader.mount_cached_question(q)
 		if !success:
 			Loader.remove_from_question_cache(q)
 			R.crash("Could not load resource pack for question ID %s. The file appears to be corrupted, so it has been removed from the cache." % q)
-		# check this file
-		# if !file.file_exists("res://q/%s/title.wav.import" % q):
-		# 	R.crash("Loaded resource pack for question ID %s, but it has not been correctly extracted." % q + "Cause of failure: res://q/%s/title.wav.import does not exist." % q)
+		# check required files
+		var file = File.new()
+		if !file.file_exists("res://q/%s/title.wav.import" % q):
+			R.crash("Loaded resource pack for question ID %s, but it has not been correctly extracted.\n" % q + "Cause of failure: res://q/%s/title.wav.import does not exist." % q)
+			return
 		if !file.file_exists("res://q/%s/_question.gdcfg" % q):
-			R.crash("Loaded resource pack for question ID %s, but it seems to be incomplete." % q + "Cause of failure: res://q/%s/_question.gdcfg does not exist." % q)
-		# else:
-		# 	file.open("res://q/%s/_question.gdcfg" % q, File.READ)
-		# 	file.close()
+			R.crash("Loaded resource pack for question ID %s, but it seems to be incomplete.\n" % q + "Cause of failure: res://q/%s/_question.gdcfg does not exist." % q)
+			return
+		print("_load_question(%s): " % q, "Loading question text preemptively...")
+		var result = Loader.load_question_text(q)
+		if result:
+			R.crash(("Loading question text for %s" % q) + " failed with error code %d." % result)
+			return
 #		# check last file
 #		q = R.pass_between.episode_data.question_id[0]
 #		if !file.file_exists("res://q/%s/title.wav.import" % q):
@@ -322,7 +308,52 @@ func _load_question(q):
 #			file.open("res://q/%s/_question.gdcfg" % q, File.READ)
 #			LOG_FILE.store_line("# First question content is now:\n" + file.get_as_text())
 #			file.close()
-		emit_signal("next_question_please")
+		print("_load_question(%s): " % q, "Question text loaded preemptively. ", len(Loader.question_texts[q].keys()), " keys in total")
+		print("Emitting signal next_question_please")
+		call_deferred("emit_signal", "next_question_please")
 	else:
 		R.crash("Tried to load the assets for question ID " + q + ", but the asset pack does not exist. Please try clearing your question cache and try again.")
 		return
+
+
+# Request the game to start.
+func start_game():
+	if !question_pack_is_downloaded:
+		# Wait until the question files are loaded.
+		# This function will be called again once it's done.
+		waiting_for_game_start = true
+		current_page.get_node("MouseMask").color = Color(0, 0, 0, 0.5)
+		current_page.get_node("LoadingPanel").show()
+		return
+	# Fade the music. (Takes 0.5 seconds.)
+	S.play_track(0, 0.1)
+	S.play_track(1, 0.1)
+	S.play_track(2, 0.1)
+	# Transition effect.
+	#self.rect_pivot_offset = Vector2(640,360)
+	tween.interpolate_property(self, "modulate", Color.white, Color.black, 0.5, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	tween.interpolate_property(self, "rect_scale", Vector2.ONE, Vector2.ONE * 1.1, 0.5, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	tween.interpolate_property(current_page.get_node("LoadingPanel"), "rect_scale", Vector2.ONE, Vector2.ONE * 1.25, 0.5, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	tween.start()
+	yield(tween, "tween_all_completed")
+	get_tree().call_deferred("change_scene", "res://Episode.tscn")
+
+# show player how much data is loaded
+func _update_loading_progress(partial, load_count):
+	var time = OS.get_ticks_msec()
+	# total time / total size == partial time / partial size.
+	# move total size to right hand side and get
+	# total time == partial time * total size / partial size..
+	var eta = -1
+	if partial != 0:
+		eta = (
+			1.0 * (time - time_start) * # partial time
+			(load_count - load_start) / # total size
+			(partial - load_start) # partial size
+		) - (time - time_start)  # subtract elapsed time from total
+	current_page.update_loading_progress(
+		partial + QUESTION_COUNT - load_count,
+		QUESTION_COUNT,
+		int(eta)
+	)
+
