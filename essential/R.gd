c236e2b2c391cd2bcc9737c6d71186ffddad07ce
JapanYoshi <japanyoshi777@gmail.com>
2023-12-07 15:04:16 +0900

    Bump version

--- essential/R.gd
+++ essential/R.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const VERSION_CODE = "12-06-ARGUE-REALIZE"
+const VERSION_CODE = "12-07-COVER-SLIDING"
 
 ### "Root", for data that every page should have.
 signal change_audience_count(audience_count)
